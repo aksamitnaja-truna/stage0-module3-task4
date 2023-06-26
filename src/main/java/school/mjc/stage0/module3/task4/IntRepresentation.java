7725985e3391ba5590d91545e0f4cd0eb7ecd962
aksamitnaja-truna <r.kovalkov04@gmail.com>
2023-06-26 21:54:45 +0300

    Some addictions

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 99, h = 104, a = 97, r = 114;
+        System.out.println((char)c + "\n" + (char)h + "\n" +  (char)a + "\n" +  (char)r);
     }
 }
